152e874eb0cadb1375f01259b41491ef08062a81
Antoine Chouinard <antoine.brochu@me.com>
2018-09-28 22:36:25 -0400

    Docteur de la damnation

---
+++ doctordoom/src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+
+<style lang="scss">
+
+
+$navBorder: 5px solid blue;
+nav{
+  border: navBorder;
+}
+.about{
+  color: rosybrown;
+
+  h1{
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  }
+  }
+</style>
+
